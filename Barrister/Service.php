72eefaeb8287a7ef91bdfd068c068a12f5adaf56
Brian Tovar <brian.tovar@creditkarma.com>
2013-10-29 16:12:13 -0700

    CK-13329 updating comments

--- Barrister/Service.php
+++ Barrister/Service.php
@@ -1,6 +1,9 @@
 <?php
 /**
- * Interface to be used for the Marker/Tag Pattern
+ * A Service is just a tag interface that the Handler uses along with a Contract to process Requests. A Service must
+ * adhere to the interfaces defined within a Contract.
+ *
+ * Uses Marker/tag interface pattern
  */
 
 namespace Barrister;

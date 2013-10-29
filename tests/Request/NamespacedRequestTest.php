533d4375c72e1a81e5cdd1dc8c595f211c1432ac
Brian Tovar <brian.tovar@creditkarma.com>
2013-10-28 17:57:47 -0700

    CK-13329 adding test for namespaced request

--- tests/Request/NamespacedRequestTest.php
+++ tests/Request/NamespacedRequestTest.php
@@ -2,13 +2,37 @@
 
 class NamespacedRequestTest extends PHPUnit_Framework_TestCase {
 
-    const TEST_METHOD = 'CK.TestMethod';
-
     public function setUp() {
 
     }
 
-    public function testIsValid() {
-        $request = new Barrister\Request\NamespacedRequest(self::TEST_METHOD, array());
+    /**
+     * @dataProvider provideInvalidMethods
+     * @param $method
+     */
+    public function testIsValidWithBadMethods($method) {
+        $request = new Barrister\Request\NamespacedRequest($method, array());
+        $this->assertFalse($request->isValid());
+    }
+
+    public function provideInvalidMethods() {
+        return array(
+            array('NamespaceOnly'), array('Namespace.InterfaceOnly'), array('Namespace\Interface\Method')
+        );
+    }
+
+    /**
+     * @dataProvider provideValidMethods
+     * @param $method
+     */
+    public function testIsValidWithGoodMethods($method) {
+        $request = new Barrister\Request\NamespacedRequest($method, array());
+        $this->assertTrue($request->isValid());
+    }
+
+    public function provideValidMethods() {
+        return array(
+            array('Namespace.Interface.Method'), array('NamespaceSegment.NamespaceSegment.Interface.Method')
+        );
     }
 }

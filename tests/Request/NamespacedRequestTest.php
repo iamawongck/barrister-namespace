02e638a5fc50442afb2b46bdbfec5d9837260730
Brian Tovar <brian.tovar@creditkarma.com>
2013-10-28 18:20:50 -0700

    CK-13329 adding tests for namespaced request

--- tests/Request/NamespacedRequestTest.php
+++ tests/Request/NamespacedRequestTest.php
@@ -2,9 +2,9 @@
 
 class NamespacedRequestTest extends PHPUnit_Framework_TestCase {
 
-    public function setUp() {
-
-    }
+    const NAMESPACE_NAME = 'Namespace';
+    const INTERFACE_NAME = 'Interface';
+    const FUNCTION_NAME = 'Method';
 
     /**
      * @dataProvider provideInvalidMethods
@@ -34,5 +34,33 @@ class NamespacedRequestTest extends PHPUnit_Framework_TestCase {
         return array(
             array('Namespace.Interface.Method'), array('NamespaceSegment.NamespaceSegment.Interface.Method')
         );
+    }
+
+    public function testGetNamespace() {
+        $request = $this->getValidRequest();
+        $this->assertEquals(self::NAMESPACE_NAME, $request->getNamespace());
+    }
+
+    public function testGetInterface() {
+        $request = $this->getValidRequest();
+        $this->assertEquals(self::INTERFACE_NAME, $request->getInterface());
+    }
+
+    public function testGetFunction() {
+        $request = $this->getValidRequest();
+        $this->assertEquals(self::FUNCTION_NAME, $request->getFunction());
+    }
+
+    public function testGetMethod() {
+        $request = $this->getValidRequest();
+        $this->assertEquals($this->getMethod(), $request->getMethod());
+    }
+
+    private function getValidRequest() {
+        return new Barrister\Request\NamespacedRequest($this->getMethod(), array());
+    }
+
+    private function getMethod() {
+        return self::NAMESPACE_NAME . '.' . self::INTERFACE_NAME . '.' . self::FUNCTION_NAME;
     }
 }
